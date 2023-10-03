df6df08bffe48d9c578883e5792e0a19e993998f
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2023-10-03 14:51:12 +0530

    Add files via upload

---
+++ JAVA/Classcode/dataStructure/4Array/p1.java
@@ -0,0 +1,21 @@
+
+
+//geven an array and print all element
+
+
+class Demo{
+
+	public static void main(String[] boss){
+	
+	
+		int arr[] = new int[]{32,43,32,44,2,3};			int N = arr.length;
+
+		for(int i = 0;i<N;i++){
+		
+			System.out.print( arr[i]+" ");
+		
+		}
+	
+	}
+
+}
